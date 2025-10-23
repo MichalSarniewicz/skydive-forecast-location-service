581ba7612801d610a345b042da540202d9356caf
MichalSarniewicz <MichalSarniewicz@users.noreply.github.com>
2025-10-23 08:37:00 +0200

    fix: change the schema name to skydive_forecast_location in the init-db file

--- init-db.sql
+++ init-db.sql
@@ -1,11 +1,11 @@
 -- Create a schema for the application
-CREATE SCHEMA IF NOT EXISTS "skydive-forecast-location";
+CREATE SCHEMA IF NOT EXISTS skydive_forecast_location;
 
--- Grant privileges to the application location
-GRANT ALL PRIVILEGES ON SCHEMA "skydive-forecast-location" TO skydive_forecast_location;
-GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA "skydive-forecast-location" TO skydive_forecast_location;
-GRANT ALL PRIVILEGES ON ALL SEQUENCES IN SCHEMA "skydive-forecast-location" TO skydive_forecast_location;
+-- Grant privileges to the application user
+GRANT ALL PRIVILEGES ON SCHEMA skydive_forecast_location TO skydive_forecast_location;
+GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA skydive_forecast_location TO skydive_forecast_location;
+GRANT ALL PRIVILEGES ON ALL SEQUENCES IN SCHEMA skydive_forecast_location TO skydive_forecast_location;
 
 -- Set default privileges for future tables
-ALTER DEFAULT PRIVILEGES IN SCHEMA "skydive-forecast-location" GRANT ALL PRIVILEGES ON TABLES TO skydive_forecast_location;
-ALTER DEFAULT PRIVILEGES IN SCHEMA "skydive-forecast-location" GRANT ALL PRIVILEGES ON SEQUENCES TO skydive_forecast_location;
+ALTER DEFAULT PRIVILEGES IN SCHEMA skydive_forecast_location GRANT ALL PRIVILEGES ON TABLES TO skydive_forecast_location;
+ALTER DEFAULT PRIVILEGES IN SCHEMA skydive_forecast_location GRANT ALL PRIVILEGES ON SEQUENCES TO skydive_forecast_location;
